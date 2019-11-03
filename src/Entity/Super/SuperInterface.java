65d5e0ea5d6c144a3586adff3692f78ff6c021c1
wangyao <3050740551@qq.com>
2019-11-03 23:31:33 +0800

    超级用户接口

--- src/Entity/Super/SuperInterface.java
+++ src/Entity/Super/SuperInterface.java
@@ -1,4 +1,11 @@
 package Entity.Super;
 
+import Entity.Admini.Admini;
+
 public interface SuperInterface {
+    void addadmini(Admini admini);
+
+    void delete(Admini admini);
+
+    void close();
 }
