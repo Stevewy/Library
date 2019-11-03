40a4b12b23fc68fe7bcbcd5963cf7f601861c345
wangyao <3050740551@qq.com>
2019-11-03 23:28:12 +0800

    普通用户接口

--- src/Entity/Student/StudentInterface.java
+++ src/Entity/Student/StudentInterface.java
@@ -1,4 +1,13 @@
 package Entity.Student;
 
+import Entity.Book.Book;
+
 public interface StudentInterface {
+    void borrow(Book book);
+
+    void returnbook(Book book);
+
+    void look();
+
+    void change();
 }
