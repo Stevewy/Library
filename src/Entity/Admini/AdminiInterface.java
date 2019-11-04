9e0954efdf8a71a2de9a941b47f87dbe27a20bf8
18871657690@163.com <18871657690@163.com>
2019-11-04 13:20:56 +0800

    你看看

--- src/Entity/Admini/AdminiInterface.java
+++ src/Entity/Admini/AdminiInterface.java
@@ -1,8 +1,9 @@
 package Entity.Admini;
-
+import DAO.Book.Book;
+import Entity.Student.Student;
 public interface AdminiInterface {
-    void creatAccount();
-    void deleteAccount();
-    void increaseBook();
-    void deleteBook();
+    void creatAccount(Student student);
+    void deleteAccount(Student student);
+    void increaseBook(Book book);
+    void deleteBook(Book book);
 }
