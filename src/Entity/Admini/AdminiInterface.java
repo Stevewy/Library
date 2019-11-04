af773e7523df2649e33e87fa214bfd7fa298406d
wangyao <3050740551@qq.com>
2019-11-04 21:19:48 +0800

    用户接口

---
+++ src/Entity/Admini/AdminiInterface.java
@@ -0,0 +1,21 @@
+package Entity.Admini;
+import Entity.Book.Book;
+import Entity.Student.Student;
+/**
+ * @author Zengfanyu
+ *  @date 2019/11/4
+ *  @function
+ */
+public interface AdminiInterface {
+    /**
+     * @
+      * @param student
+     */
+    void creatAccount(Student student);
+
+    void deleteAccount(Student student);
+
+    void increaseBook(Book book);
+
+    void deleteBook(Book book);//
+}
