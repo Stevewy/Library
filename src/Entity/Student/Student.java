15500a21b04dc089a00c9d2c543b7ecd36a2ae18
wangyao <3050740551@qq.com>
2019-11-13 19:41:01 +0800

    无

--- src/Entity/Student/Student.java
+++ src/Entity/Student/Student.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 /**
  * @author WangYao
  * @date 2019/11/4
- * @function 学生实现类
+ * @function 学生实现类  
  */
 
 public class Student implements StudentInterface, Serializable {
