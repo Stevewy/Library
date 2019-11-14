979f81d714d4319a685e6e5cad865938bf7b4ae1
wangyao <3050740551@qq.com>
2019-11-14 19:16:10 +0800

    无

--- src/Entity/Student/Student.java
+++ src/Entity/Student/Student.java
@@ -17,6 +17,7 @@ public class Student implements StudentInterface, Serializable {
     private String password;
     private ArrayList<Book> books = new ArrayList<>();
     private ArrayList<Integer> number = new ArrayList<>();
+    private StudentDaoBook studentDaoBook = new StudentDaoBook();
 
     public Student(String account, String password) {
         this.account = account;
@@ -38,6 +39,7 @@ public class Student implements StudentInterface, Serializable {
     public boolean changePassword(String password){
         if(!isChinese(password)) {
             this.password = password;
+
             return true;
         }
         else return false;
@@ -50,45 +52,56 @@ public class Student implements StudentInterface, Serializable {
     }
 
     public void lookAllBook(){
-        printf(StudentDaoBook.lookAllBook());
+        printf(studentDaoBook.lookAllBook());
     }
 
     private void printf(ArrayList<Book> b ){
         for(int i = 0;i < b.size();i++){
                 System.out.print(b.get(i)+"       ");
-                if((i + 1) % 3 == 0&&i != b.size() - 1) System.out.println();      // 打印3本书后换行
+                if((i + 1) % 3 == 0 || i == b.size() - 1) System.out.println();      // 打印3本书后换行
             }
             System.out.println();
     }
 
     public boolean lookBookByKind(String kind){
-        if(StudentDaoBook.searchBookByKind(kind) != null){
-            printf(StudentDaoBook.searchBookByKind(kind));
+        if(studentDaoBook.searchBookByKind(kind) != null){
+            printf(studentDaoBook.searchBookByKind(kind));
             return true;
         }
         else return false;
     }
 
     public boolean lookBookByName(String name){                                //如果找到就返回ture 否则返回false
-        if(StudentDaoBook.searchBookByName(name) != null){
-            System.out.println(StudentDaoBook.searchBookByName(name));
+        if(studentDaoBook.searchBookByName(name) != null){
+            printf(studentDaoBook.searchBookByName(name));
             return true;
         }
         else return false;
     }
 
     public boolean lookBookByBookNumber(String bookNumber){                    //如果找到就返回ture 否则返回false
-        if(StudentDaoBook.searchBookByBookNumber(bookNumber) != null){
-            System.out.println(StudentDaoBook.searchBookByBookNumber(bookNumber));
+        if(studentDaoBook.searchBookByBookNumber(bookNumber) != null){
+            System.out.println(studentDaoBook.searchBookByBookNumber(bookNumber));
             return true;
         }
         else return false;
     }
 
-    public boolean borrowBook(Book book,int number){                          //如果借出成功返回ture 否则返回false
-        if(book.lentOut(number)){
-            this.books.add(book);
-            this.number.add(number);
+    public boolean borrowBook(Book book,int number){                                      //如果借出成功返回ture 否则返回false
+        if(book.getNowAmount() > number){
+            book.setNowAmount(book.getNowAmount() - number);
+            book.setLentAmount(book.getLentAmount() + number);
+            int i;
+            for(i =0; i < books.size(); i++){
+                if(books.get(i).equals(book)){
+                    this.number.set(i,this.number.get(i) + number);
+                }
+            }
+            if(i == books.size()){
+                this.books.add(book);
+                this.number.add(number);
+            }
+            studentDaoBook.updateBook(book);
             return true;
         }
         else return false;
@@ -108,7 +121,7 @@ public class Student implements StudentInterface, Serializable {
                         "   出版社:" + books.get(i).getPress() +
                         "   种类" + books.get(i).getKind() +
                         "   借了" + number.get(i) + "本");
-                if((i + 1) % 3 == 0&&i != books.size() - 1) System.out.println();              // 打印3本书后换行
+                if((i + 1) % 3 == 0 || i == books.size() - 1) System.out.println();              // 打印3本书后换行
             }
         }
     }
@@ -123,7 +136,7 @@ public class Student implements StudentInterface, Serializable {
                         "   出版社:" + books.get(i).getPress() +
                         "   种类" + books.get(i).getKind() +
                         "   借了" + number.get(i) + "本");
-                if((i + 1) % 3 == 0&&i != books.size() - 1) System.out.println();              // 打印3本书后换行
+                if((i + 1) % 3 == 0 || i == books.size() - 1) System.out.println();              // 打印3本书后换行
                 else System.out.print("          ");
             }
         }
