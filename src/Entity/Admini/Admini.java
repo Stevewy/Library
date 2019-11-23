880a7d9af60da16e82ddd966710dc926adfea629
18871657690@163.com <18871657690@163.com>
2019-11-23 18:16:45 +0800

    ok

--- src/Entity/Admini/Admini.java
+++ src/Entity/Admini/Admini.java
@@ -4,10 +4,7 @@ import Dao.DaoUser.DaoAdministrator;
 import Entity.Book.Book;
 import Entity.Student.Student;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,55 +13,50 @@ import java.util.Scanner;
  * @date 2019/11/4
  * @function
  */
-public class Admini  implements AdminiInterface {
-    static Scanner in=new Scanner(System.in);
+public class Admini   {
+    private String account;
+    private String password;
 
-    public void createAccount(){
-        System.out.println("请输入新账号:\n");
-        String account =in.next();
-        try {
-            if(DaoAdministrator.createStudent(account)){
-                System.out.println("请输入密码:\n");
-                String password=in.next();
-                Student student=new Student(account,password);
-                ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("Students/"+account+".txt") );
-                oos.writeObject(student);
-                System.out.println("创建成功\n");
-                oos.close();
-            }
-            else {
-                System.out.println("Error!!!");
-            }
-        }catch (Exception e){
-        e.printStackTrace();
-        }
+    public boolean createAccount(String account,String password){
+           if(!DaoAdministrator.studentExist(account)) {
+               Student student = new Student(account, password);
+               if(DaoAdministrator.creatStudent(student))
+                   return true;
+               else
+                   return false;
+           }
+           else
+               return false;
     }
 
+    public boolean deleteAccount(String account) {
+        if(DaoAdministrator.studentExist(account)){
+            if( DaoAdministrator.deleteStudent(account)){
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
 
-    public static void searchStudent(){
-        System.out.println("请输入查找的账号:\n");
-        String account=in.next();
+    public static void searchStudent(String account){
         Student student= DaoAdministrator.searchStudent(account);
         if(student!=null){
         System.out.println("账户信息如下");
         System.out.println(student.getAccount()+" "+" "+student.getPassword());
-        System.out.println(student.getBooks());
-        }
+        System.out.println(student.getBooks()+" ");
+        System.out.println(student.getNumber()+" ");
+    }
         else
             System.out.println("该账户不存在");
     }
 
+    public void addBook(Book book, int number){}
 
-    public void deleteAccount() {
-        System.out.println("请输入要删除的账号");
-        String account =in.next();
-        Student student =DaoAdministrator.deleteStudent(account);
+    public void deleteBook(Book book,int number)throws FileNotFoundException,IOException{
 
     }
-
-    public void increaseBook(Book book, int number){
-
-    }
-    public void deleteBook(Book book){}
     
 }
