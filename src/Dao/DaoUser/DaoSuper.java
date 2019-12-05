bd7b11d66fbce4510adc2190fc5d3d4c77ca48a9
18871657690@163.com <18871657690@163.com>
2019-12-05 20:59:24 +0800

    delete

--- src/Dao/DaoUser/DaoSuper.java
+++ src/Dao/DaoUser/DaoSuper.java
@@ -13,96 +13,103 @@ import java.util.ArrayList;
  * @function
  */
 public class DaoSuper {
-    public static boolean createAdministrator(Admini admini){
-        File administrator = new File("Administrator/"+admini.getAccount()+".txt");
+    public static boolean createAdministrator(Admini admini) {
+        File administrator = new File("Administrator/" + admini.getAccount() + ".txt");
         if (administrator.exists()) {
             return false;
-        }
-        else {
+        } else {
             try {
-                ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(administrator));
+                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(administrator));
                 oos.writeObject(admini);
                 return true;
-            }
-           catch (IOException ie){
+            } catch (IOException ie) {
                 ie.printStackTrace();
                 return false;
             }
         }
     }
 
-    public static boolean deleteAdministrator(Admini admini){
-        File administrator =new File ("Administrator/"+admini.getAccount()+".txt");
+    public static boolean deleteAdministrator(Admini admini) {
+        File administrator = new File("Administrator/" + admini.getAccount() + ".txt");
         if (administrator.exists()) {
             administrator.delete();
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public static ArrayList<Admini> viewAllAdministratorInfo(){
-        File file =new File("Administrator");
-        String info[]=file.list();
-        ArrayList<Admini>adm=new ArrayList<>();
-        try{
-        for(String names:info){
-            ObjectInputStream ois =new ObjectInputStream(new FileInputStream("naems"));
-            adm.add((Admini)ois.readObject());
-        }
-        return adm;
-     }
-        catch (FileNotFoundException fe){
+    public static ArrayList<Admini> viewAllAdministratorInfo() {
+        File file = new File("Administrator");
+        String info[] = file.list();
+        ArrayList<Admini> adm = new ArrayList<>();
+        try {
+            for (String names : info) {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream("naems"));
+                adm.add((Admini) ois.readObject());
+            }
+            return adm;
+        } catch (FileNotFoundException fe) {
             System.out.println("不能找到相关文件");
             return null;
-        }
-        catch (IOException ioe){
+        } catch (IOException ioe) {
             System.out.println("文件读取错误");
             return null;
-        }
-        catch (ClassNotFoundException cle){
+        } catch (ClassNotFoundException cle) {
             System.out.println("文件读取错误");
             return null;
         }
     }
-
 
     public static Admini searchAdministrator(String account) {
         File admini = new File("Administrator/" + account + ".txt");
-        if (admini.exists()){
+        if (admini.exists()) {
             try {
-               ObjectInputStream in = new ObjectInputStream(new FileInputStream(admini));
-               Admini adm = (Admini) in.readObject();
-               in.close();
-               return adm;
-            }
-            catch (FileNotFoundException ce){
+                ObjectInputStream in = new ObjectInputStream(new FileInputStream(admini));
+                Admini adm = (Admini) in.readObject();
+                in.close();
+                return adm;
+            } catch (FileNotFoundException ce) {
                 System.out.println("找不到文件");
-                 return null;
-            }
-            catch (Exception e) {
+                return null;
+            } catch (Exception e) {
                 System.out.println("文件读取失败");
                 return null;
             }
-        }
-        else
+        } else
             return null;
     }
 
-    public static boolean updateSuper(Super sup){
-        try{
-            ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream(sup.getAccount()+".txt"));
+    public static boolean updateSuper(Super sup) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(sup.getAccount() + ".txt"));
             oos.writeObject(sup);
             return true;
-        }
-        catch (FileNotFoundException fe){
+        } catch (FileNotFoundException fe) {
             System.out.println("超级管理员文件查找失败");
             return false;
-        }
-        catch (IOException ioe ){
+        } catch (IOException ioe) {
             System.out.println("文件读取失败");
             return false;
         }
+    }
+
+    public static Super viewSelf(String account) {
+        File file = new File("Super/" + account + ".txt");
+        if (file.exists()) {
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+                Super sup = (Super) ois.readObject();
+                return super;
+
+            } catch (FileNotFoundException fe) {
+                System.out.println("找不到该文件");
+                return null;
+            } catch (Exception ioe) {
+                System.out.println("读取文件失败");
+                return null;
+            }
+        } else
+            return null;
     }
 }
