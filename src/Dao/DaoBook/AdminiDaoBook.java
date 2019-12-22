fb37fb91ded0c52a71b13d3e1ff516966cbc2e6e
wangyao <3050740551@qq.com>
2019-12-22 18:55:52 +0800

    ?

--- src/Dao/DaoBook/AdminiDaoBook.java
+++ src/Dao/DaoBook/AdminiDaoBook.java
@@ -21,7 +21,7 @@ public class AdminiDaoBook extends StudentDaoBook {
 
     public void sortBookAndPrint(){
         Collections.sort(books, new BookCompar());
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 5 && i < books.size(); i++)
             System.out.println("\t" + books.get(i) + "\t   总共借出:" + books.get(i).getLentAmount() + "次");
 
     }
